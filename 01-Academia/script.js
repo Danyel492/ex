cb88037787e07c1967574eb9d4fed21533b9a855
Danyel492 <danyelguto@gmail.com>
2025-01-07 16:13:34 -0300

    inicio dos exercicios

---
+++ 01-Academia/script.js
@@ -0,0 +1,13 @@
+/*
+Escreva uma função que recebe um numero qualquer de numeros inteiros como argumentos e retorne a média aritimetica entre eles
+*/
+
+function mediaAritimetica(...numeros) {
+  let soma = 0
+  for (let numero of numeros) {
+    soma += numero
+  }
+  return soma / numeros.length
+
+}
+console.log(mediaAritimetica(25,75))
