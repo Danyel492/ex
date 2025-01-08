7ca829c7c6e773ba9072cb55194a3c49af685893
Danyel492 <danyelguto@gmail.com>
2025-01-08 12:13:56 -0300

    fim do very easy

--- 06-Historia-escondida-II/ex6.js
+++ 06-Historia-escondida-II/ex6.js
@@ -1,18 +1,17 @@
 function decifrar(frase) {
-
-    let palavras = frase.split(" ")
+    let invertida = []
+    let minusculo = frase.toLowerCase()
+    let palavras = minusculo.split(" ")
     for (let i = palavras.length -1; i >= 0; i--){
-        let palavra = 
+        invertida.push(palavras[i].split('').reverse().join(''))
     }
+    invertida = invertida.reverse()
 
-    return palavras
+    return invertida.join(' ')
 }
 
-console.log(decifrar("Danyel Augusto Araujo Silva")) // leynad otsugua ojuara avlis
 
-/*
 console.log(decifrar("Lorem ipsum dolore sec avanti")) // merol muspi erolod ces itnava
 console.log(decifrar("This is an apple")) // siht si na elppa
 console.log(decifrar("May the force be with you")) // yam eht ecrof eb htiw uoy
 console.log(decifrar("It s over nine thousand")) // ti s revo enin dnasuoht
-*/
