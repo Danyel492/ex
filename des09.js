1ea3109c819d4799d61f42bec584d0f1d38799b2
Danyel492 <danyelguto@gmail.com>
2025-01-10 15:33:25 -0300

    inicio da 10

--- des09.js
+++ des09.js
@@ -1,11 +1,22 @@
-function isUniform(frase){
-    let tudo = frase.toLowerCase().replace(/\s/g,'').split('').sort()
-
-    let letra = tudo[0]
-
-    return letra
+function balanceado(frase) {
+    let caracteresOrdenados = frase.toLowerCase().replace(/\s/g, '').split('').sort()
+    let verificar = 0
+    if (caracteresOrdenados.length % 2 === 1) {
+        return false
+    } else {
+        for (let i = 0; i <= caracteresOrdenados.length - 1; i+=2) {
+            if (caracteresOrdenados[i] !== caracteresOrdenados[i + 1]){
+                verificar ++
+            }
+        }
+        if (verificar === 0){
+            return true
+        }
+        return false
+    }
 }
 
-console.log(isUniform('This is Thee')) // true
-console.log(isUniform('ssd')) // false
-console.log(isUniform('aabbcc')) // true
+console.log(balanceado('This is Thee')) // true
+console.log(balanceado('ssd')) // false
+console.log(balanceado('aabbcc')) // true
+console.log(balanceado('aabbccty')) // false
---
+++ des10.js
@@ -0,0 +1,12 @@
+function fatorar(num) {
+    if (num === 0 || num === 1) {
+        return 1
+    }
+    return num
+    
+}
+
+console.log(fatorar(3)) // 6
+console.log(fatorar(4)) // 24
+console.log(fatorar(5)) // 120
+console.log(fatorar(6)) // 720
